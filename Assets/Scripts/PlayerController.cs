4e76b30d53baa04396ac81386f530e4581086cb6
Henry Willams <nthphuc@apcs.vn>
2019-08-18 15:31:08 +0700

    AI created

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -7,19 +8,45 @@ public class PlayerController : MonoBehaviour
 {
     public Camera cam;
 
+    public GameObject player;
+
     public NavMeshAgent agent;
+
+    private float _x, _y, _z;
     // Update is called once per frame
+    private void Start()
+    {
+        _x = gameObject.transform.position.x;
+        _y = gameObject.transform.position.y;
+        _z = gameObject.transform.position.z;
+    }
+
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Vector3.Distance(player.transform.position,gameObject.transform.position)<20)
         {
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
+            agent.SetDestination(player.transform.position);
+        }
+        else
+        {
+            Example();
+        }
+    }
 
-            if (Physics.Raycast(ray, out hit))
-            {
-                agent.SetDestination(hit.point);
-            }
-        }   
+    public void Example()
+    {
+        _y = 0.44f;
+        Debug.Log(new Vector3(_x, _y, _z));
+        Debug.Log(gameObject.transform.position);
+        Debug.Log(Vector3.Distance(new Vector3(_x, _y, _z), gameObject.transform.position));
+        if (Vector3.Distance(new Vector3(_x, _y, _z), gameObject.transform.position) == 0)
+        {
+            _y = -0.44f;
+            _x = UnityEngine.Random.Range(-8.0f, 88.0f);
+            _z = UnityEngine.Random.Range(-10.0f, 86.0f);
+            agent.SetDestination(new Vector3(_x, _y, _z));
+            Debug.Log(new Vector3(_x, _y, _z));
+        }
+
     }
 }
