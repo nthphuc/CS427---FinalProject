f74c2e2c205968059780ea0b402dd30d42fdfd07
Nguyễn Trung Nghĩa <ntnghia@apcs.vn>
2019-08-20 17:34:50 +0700

    add vibration effect and main menu

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+
+    public GameObject player;
+
+    public NavMeshAgent agent;
+
+    private float _x, _y, _z;
+    // Update is called once per frame
+    private void Start()
+    {
+        _x = gameObject.transform.position.x;
+        _y = gameObject.transform.position.y;
+        _z = gameObject.transform.position.z;
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(player.transform.position,gameObject.transform.position)<20)
+        {
+            agent.SetDestination(player.transform.position);
+        }
+        else
+        {
+            Example();
+        }
+    }
+
+    public void Example()
+    {
+        _y = gameObject.transform.position.y;
+        agent.SetDestination(new Vector3(_x, _y, _z));
+        Debug.Log(Vector3.Distance(new Vector3(_x, _y, _z), gameObject.transform.position));
+        if (Vector3.Distance(new Vector3(_x, _y, _z), gameObject.transform.position) <= 1.2f)
+        {
+            _y = -0.44f;
+            _x = UnityEngine.Random.Range(-8.0f, 88.0f);
+            _z = UnityEngine.Random.Range(-10.0f, 86.0f);
+            agent.SetDestination(new Vector3(_x, _y, _z));
+            // Debug.Log(new Vector3(_x, _y, _z));
+        }
+
+    }
+}
---
+++ Assets/ShakeCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShakeCamera : MonoBehaviour
+{
+    public CameraShake cameraShake;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentDis = Vector3.Distance(transform.position, Camera.main.gameObject.transform.position);
+        Debug.Log("dis: " + currentDis);
+        if (currentDis < 4f){
+            StartCoroutine(cameraShake.Shake(.15f, .4f));
+            Debug.Log("shaking" + currentDis);
+        }
+    }
+}
