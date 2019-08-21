facfbf0c70e2968f83dcb4513bee012b8e4ade19
Nguyễn Trung Nghĩa <ntnghia@apcs.vn>
2019-08-21 23:50:02 +0700

    add two items

---
+++ Assets/DieWhenBeKilled.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DieWhenBeKilled : MonoBehaviour
+{
+    public KillGhost killGhost;
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.name == "FPSController" && killGhost.KillerStatus == true)
+        {
+            gameObject.SetActive(false);
+            Debug.Log("BE KILLED");
+        }
+    }
+}
